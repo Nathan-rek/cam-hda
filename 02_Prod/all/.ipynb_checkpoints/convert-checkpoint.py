11256602de8ceb188d83e8c84084a3ec1f99f44b
Nathan-rek <130574828+Nathan-rek@users.noreply.github.com>
2024-11-22 17:10:30 +0100

    quanedgtpu

--- 02_Prod/all/.ipynb_checkpoints/convert-checkpoint.py
+++ 02_Prod/all/.ipynb_checkpoints/convert-checkpoint.py
@@ -2,10 +2,9 @@ from ultralytics import YOLO
 
 # Load the YOLO11 model
 model = YOLO("judd.pt")
-model = YOLO("judd.pt")
 
 # Export the model to TFLite format
-model.export(format="edgetpu")  # creates 'yolo11n_float32.tflite'
+model.export(format="tflite")  # creates 'yolo11n_float32.tflite'
 
 # Load the exported TFLite model
 tflite_model = YOLO("yolo11n_float32.tflite")
---
+++ 02_Prod/rohtko2/02_Prod/main.py
@@ -0,0 +1,14 @@
+from ultralytics import YOLO
+import torch
+
+# Vérifiez si un GPU est disponible
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
+print(f'Device: {device}')
+
+# Chargez le modèle
+model = YOLO("yolov8n.yaml")
+
+# Entraînez le modèle en utilisant le GPU
+model.train(data="config.yaml", epochs=75)
+
+print(epochs)
