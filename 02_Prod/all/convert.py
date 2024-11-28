53bbbc6265cce7a459015e8689eb1347ac3bdfab
Nathan-rek <130574828+Nathan-rek@users.noreply.github.com>
2024-11-28 15:14:00 +0100

    maj

--- 02_Prod/all/convert.py
+++ 02_Prod/all/convert.py
@@ -1,14 +1,9 @@
 from ultralytics import YOLO
 
 # Load the YOLO11 model
-model = YOLO("judd.pt")
-model = YOLO("judd.pt")
+model = YOLO("rothko.pt")
 
 # Export the model to TFLite format
-model.export(format="edgetpu")  # creates 'yolo11n_float32.tflite'
+model.export(format="ncnn")  # creates 'yolo11n_float32.tflite'
 
-# Load the exported TFLite model
-tflite_model = YOLO("yolo11n_float32.tflite")
 
-# Run inference
-results = tflite_model("https://ultralytics.com/images/bus.jpg")
